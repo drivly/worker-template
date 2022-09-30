f917c6408505a8b8fe4e55cf7b37718ccb18d836
Nathan Clevenger <nateclev@gmail.com>
2022-09-30 08:14:50 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ export default {
   fetch: req => {
     const { url } = req
     const { hostname, pathname, searchParams } = new URL(url)
-    const [ tld, sld, subdomain ] hostname.split('.').reverse()
+    const [ tld, sld, subdomain ] = hostname.split('.').reverse()
     const headers = Object.fromEntries(req.headers)
     json({ url, hostname, tld, sld, subdomain, pathname, searchParams, headers })
   }
