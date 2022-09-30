98fc51048cc4d0d5e2a2611ae6ab0f4a8b369490
Nathan Clevenger <nateclev@gmail.com>
2022-09-30 07:38:58 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // import { json } from 'https://pkg.do/apis.do'
 
 export default {
-  fetch: req => new Response(JSON.stringify({ hello: req.cf.city }, null, 2)) 
+  fetch: req => fetch('https://api.cf') //new Response(JSON.stringify({ hello: req.cf.city }, null, 2)) 
 }
