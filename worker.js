7f15fa5bad7bb652b5a504b732acf72f70681c21
Nathan Clevenger <nateclev@gmail.com>
2022-09-30 08:17:29 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,11 +2,5 @@
 import { json } from 'http://pkg.do/itty-router-extras'
 
 export default {
-  fetch: req => {
-    const { url } = req
-    const { hostname, pathname, searchParams } = new URL(url)
-    const [ tld, sld, subdomain ] = hostname.split('.').reverse()
-    const headers = Object.fromEntries(req.headers)
-    json({ url, hostname, tld, sld, subdomain, pathname, searchParams, headers })
-  }
+  fetch: ({url}) => json({ hello: 'world', url, time: Date.now() })
 }
